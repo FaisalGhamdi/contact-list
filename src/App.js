31423713b964176a4d40d5700cabf9e66c59a078
Faisal AlGhamdi <faisal.ghamdi92@hotmail.com>
2021-06-07 10:44:19 -0500

    added logic to load data from sevrice

--- src/App.js
+++ src/App.js
@@ -6,30 +6,59 @@ import ContactForm from './components/ContactForm'
 
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { render } from '@testing-library/react';
 
+const SERVICE_URL = "https://tsg-contactlist.herokuapp.com"
 
-function App() {
-  return (
+class App extends React.Component {
+
+  state = {
+    loading: false,
+    contactData: [
+      {
+        "contactId": 1, "firstName": "Fake",
+        "lastName": "Data",
+        "company": "Unknown Inc.",
+        "phone": "000-0000",
+        "email": "fakedata@unknown.io"
+      }]
+  }
+
+  componentDidMount() {
+    console.log("App is now mounted.")
+    this.setState({ loading: true })
+    console.log("Loading contact data")
+    fetch(SERVICE_URL + "/contacts")
+      .then(data => data.json())
+      .then(data => this.setState(
+        { contactData: data, loading: false }
+      ))
+  }
+
+  render() {
+    return (
       <Container fluid>
         <Row>
           <Col>
-          <h1 className="text-center">Contact Application</h1>
+            <h1 className="text-center">Contact Application</h1>
           </Col>
         </Row>
-        <hr/>
+        <hr />
         <Row>
           <Col sm={8}>
-          <h2>My Contacts</h2>
-            <ContactTable />
+            <h2>My Contacts</h2>
+            <ContactTable contacts={this.state.contactData} />
           </Col>
           <Col sm={4}>
-          <h2>Add New Contact</h2>
+            <h2>Add New Contact</h2>
             <ContactForm />
           </Col>
         </Row>
- 
+        {/* <ContactModal /> */}
       </Container>
-  );
+    );
+  }
 }
 
 export default App;
